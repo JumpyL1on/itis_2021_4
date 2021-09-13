dc384a8ed933c832f09787e9821fbd86bf7f168c
Grigorev Yakov <yaschok315@gmail.com>
2021-09-13 22:26:44 +0300

    запись в файл результатов 2 команды

---
+++ TestService/TestService/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestService.Controllers
+{
+    [ApiController]
+    [Route("v1/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private HttpClient HttpClient { get; }
+
+        public TestController(HttpClient httpClient)
+        {
+            HttpClient = httpClient;
+        }
+
+        [HttpPost]
+        public async Task Post()
+        {
+            await using var writer = new StreamWriter("test.txt", true);
+            var url = "https://microservices-homework1.herokuapp.com/api/User/GetRolesByLoginOrEmail?login=Admin";
+            var result = await HttpClient.GetAsync(url);
+            if (result.IsSuccessStatusCode)
+            {
+                var roles = await result.Content.ReadFromJsonAsync<string[]>();
+                if (roles != null)
+                    foreach (var role in roles)
+                        await writer.WriteLineAsync(role);
+            }
+        }
+    }
+}
